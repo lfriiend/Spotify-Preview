dbccc5f92f9bd66f99726f1068b057d486d521f1
Lucas Sales <lucas.salles.oliveira@gmail.com>
2023-08-03 12:20:30 -0300

    added PlayIcon from MusicTrack

--- components/MusicTrack/MusicTrack.tsx
+++ components/MusicTrack/MusicTrack.tsx
@@ -2,6 +2,7 @@ import { MusicTrackPropsWithSelect } from "../../types/types";
 import React from "react";
 import NextImage from 'next/image';
 import { formatDuration } from '../../utils/timerConverter';
+import { PlayIcon } from '@heroicons/react/24/solid';
 
 const MusicTrack: React.FC<MusicTrackPropsWithSelect> = ({ playlist, onSelect }) => {
   const { tracks } = playlist;
@@ -26,7 +27,7 @@ const MusicTrack: React.FC<MusicTrackPropsWithSelect> = ({ playlist, onSelect })
               <span className="text-sm font-normal text-secondaryColor ">{item.track.album.artists[0].name}</span>
             </div>
           </div>
-          <span className="bg-zinc-50 w-20 h-11 rounded-2xl drop-shadow-2xl flex items-center justify-center text-textColor">{formatDuration(item.track.duration_ms)}</span>
+          <span className="bg-zinc-50 w-20 h-11 rounded-2xl drop-shadow-2xl flex items-center justify-center text-textColor"><PlayIcon className="h-5 pr-1"/>{formatDuration(item.track.duration_ms)}</span>
         </div>
       ))}
     </div>
